2faf28c4f2d13ad3b8c81324d3c156422e83c7a6
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-03-22 16:05:14 +0100

    add tcp listener with read and write to stream

--- router/src/main.rs
+++ router/src/main.rs
@@ -1,3 +1,23 @@
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
+
+fn handle_connection(stream: &mut TcpStream) {
+    let mut content_read = String::new();
+    stream.read_to_string(&mut content_read).expect("Could not parse content");
+    println!("Content:\n{}", content_read);
+    stream.write(b"HTTP/1.1 200 ok\r\n\r\nHello world").expect("Could not write to stream");
+}
+
+const ADDRESS: &str = "127.0.0.1:8080";
+
 fn main() {
-    println!("Hello, world!");
+    let address = TcpListener::bind(ADDRESS).unwrap();
+    for stream in address.incoming() {
+        match stream {
+            Ok(mut stream) => handle_connection(&mut stream),
+            Err(_) => {
+                println!("Could not handle tcp connection.");
+                return;
+            }
+        }
+    }
 }
