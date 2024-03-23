34c699aa71739f63fd09aab6c656dd4d1e2db788
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-03-23 10:14:30 +0100

    replace patterns in html with context

--- router/src/main.rs
+++ router/src/main.rs
@@ -1,13 +1,18 @@
 pub mod run;
 pub mod render;
+use std::collections::HashMap;
+
 use render::template;
 use run::{ServerOptions, Route, HTTPVerb::*, Response};
 
 fn main() {
+
     let routes = vec![
         Route::new(GET, String::from("/"), |_req| Response::new_200(String::from("Hello, World!"))),
-        Route::new(GET, String::from("/hello/"), |_req| {
-            return Response::new_200(template("my_html.html".to_string(), None));
+        Route::new(GET, String::from("/hello/"),   |_req| {
+            let mut context = HashMap::new();
+            context.insert("title".to_string(), "My website title".to_string());
+            return Response::new_200(template("my_html.html".to_string(), Some(context)));
         }),
     ];
     run::start_server(routes, ServerOptions {address: None});
--- router/src/render.rs
+++ router/src/render.rs
@@ -2,6 +2,17 @@ use std::collections::HashMap;
 
 pub fn template(path: String, context: Option<HashMap<String, String>>) -> String {
 
-    let content = std::fs::read_to_string(path).expect("Could not read file");
+    let mut content = std::fs::read_to_string(path).expect("Could not read file");
+    content = content.lines().map(|line| {
+        let mut new_line = String::from(line);
+        if new_line.contains("[- ") {
+            if let Some(context) = &context {
+                for (key, value) in context {
+                    new_line = new_line.replace(&format!("[- {} -]", key), value); 
+                }
+            }
+        }
+        new_line
+    }).collect::<Vec<String>>().join("\n");
     return content;
 }
