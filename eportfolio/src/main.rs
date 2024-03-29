59bc65ea79c2b393b44ce2a279ec83d01c9ef863
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-03-29 01:24:36 +0100

    create landing page for eportfolio

--- eportfolio/src/main.rs
+++ eportfolio/src/main.rs
@@ -1,3 +1,49 @@
+use std::collections::HashMap;
+
+use unchained::{
+    error::Error, router::{start_server, HTTPVerb::*, Response, ResponseContent, Route, ServerOptions}, templates::{
+        context::{ctx_map, ctx_str, ctx_vec}, render::template
+    }
+};
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut context = HashMap::new();
+
+    context.insert("front_links".to_string(), ctx_vec(vec![
+        ctx_map([("href", ctx_str("/#about")), ("label", ctx_str("About me"))]),
+        ctx_map([("href", ctx_str("/experience")), ("label", ctx_str("Experience"))]),
+        ctx_map([("href", ctx_str("/skills")), ("label", ctx_str("Skills"))]),
+    ]));
+
+    context.insert("carl_images".to_string(), ctx_vec(vec![
+        ctx_map([("path", ctx_str("ski.webp")), ("alt", ctx_str("Me on a randonee ski trip"))]),
+        ctx_map([("path", ctx_str("storheia.webp")), ("alt", ctx_str("Me on top of Storheia"))]),
+        ctx_map([("path", ctx_str("broa.webp")), ("alt", ctx_str("Gamlebroa"))]),
+        ctx_map([("path", ctx_str("hovedbygget.webp")), ("alt", ctx_str("Me in front of Hovedbygget NTNU"))]),
+        ctx_map([("path", ctx_str("gotland-gaard.webp")), ("alt", ctx_str("Me on Gotland"))]),
+        ctx_map([("path", ctx_str("index-intervju.webp")), ("alt", ctx_str("Me at interview with Tihlde Index"))]),
+        ctx_map([("path", ctx_str("piz-boe.webp")), ("alt", ctx_str("Me on top of Piz Boe"))]),
+    ]));
+
+
+    let start = std::time::Instant::now();
+    let template = template("templates/landing.html", Some(context));
+    let duration = start.elapsed();
+    println!("Finished rendering after {} s", duration.as_secs_f64());
+
+    let routes = vec![
+        Route::new(GET, String::from("/"), ResponseContent::Create(Box::new(move |_req| {
+            return match &template {
+                Ok(template) => Response::new_200(template.to_string()),
+                Err(e) => panic!("Error: {}", match e {
+                    Error::InvalidParams(s) => s.to_string(),
+                    Error::ParseTemplate => "What tha hell".to_string(),
+                    _ => "Unknown error".to_string(),
+                }),
+            };
+        }))),
+        Route::new(GET, String::from("/images/*"), ResponseContent::None)
+    ];
+    start_server(routes, ServerOptions {address: None});
 }
--- unchained/src/main.rs
+++ unchained/src/main.rs
@@ -1,46 +1,8 @@
-use std::collections::HashMap;
+use unchained::router::{start_server, ServerOptions};
 
-use unchained::{
-    error::Error, router::{start_server, HTTPVerb::*, Response, ResponseContent, Route, ServerOptions}, templates::{
-        context::{ctx_map, ctx_str, ctx_vec}, render::template
-    }
-};
 
 fn main() {
 
-    let mut context = HashMap::new();
-    context.insert("list".to_string(), ctx_vec(vec![
-        ctx_str("Hello"),
-        ctx_str("World"),
-    ]));
-    context.insert("title".to_string(), ctx_map([
-        ("title", ctx_str("Soek til meg pls")),
-    ]));
-
-    context.insert("front_links".to_string(), ctx_vec(vec![
-        ctx_map([("href", ctx_str("/#about")), ("label", ctx_str("About me"))]),
-        ctx_map([("href", ctx_str("/experience")), ("label", ctx_str("Experience"))]),
-        ctx_map([("href", ctx_str("/skills")), ("label", ctx_str("Skills"))]),
-    ]));
-
-
-    let start = std::time::Instant::now();
-    let template = template("gutz_html_ascii.html", Some(context));
-    let duration = start.elapsed();
-    println!("Finished rendering after {} s", duration.as_secs_f64());
-
-    let routes = vec![
-        Route::new(GET, String::from("/"), ResponseContent::Create(Box::new(move |_req| {
-            return match &template {
-                Ok(template) => Response::new_200(template.to_string()),
-                Err(e) => panic!("Error: {}", match e {
-                    Error::InvalidParams(s) => s.to_string(),
-                    Error::ParseTemplate => "What tha hell".to_string(),
-                    _ => "Unknown error".to_string(),
-                }),
-            };
-        }))),
-        Route::new(GET, String::from("/images/*"), ResponseContent::None)
-    ];
+    let routes = vec![];
     start_server(routes, ServerOptions {address: None});
 }
